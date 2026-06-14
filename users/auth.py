3c4024e141b634b486fa2c73e229d1e598bbb0b5
bendoujanna <b.vitalina@alustudent.com>
2026-06-14 23:26:00 +0200

    implement firebase jwt authentication and strict admin provisioning

---
+++ users/auth.py
@@ -0,0 +1,39 @@
+import firebase_admin
+from firebase_admin import auth, credentials
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.conf import settings
+from .models import UserProfile
+
+# initialize the Firebase Admin SDK
+if not firebase_admin._apps:
+    cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+    firebase_admin.initialize_app(cred)
+
+class FirebaseAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        # grab the authorization header
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if not auth_header:
+            return None
+
+        # token is well formatted
+        auth_header = auth_header.split()
+        if len(auth_header) != 2 or auth_header[0].lower() != 'bearer':
+            return None
+
+        id_token = auth_header[1]
+
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+            uid = decoded_token.get('uid')
+        except Exception:
+            raise AuthenticationFailed('Invalid or expired Firebase token.')
+
+        # look up the corresponding user in database
+        try:
+            user = UserProfile.objects.get(uid=uid)
+        except UserProfile.DoesNotExist:
+            raise AuthenticationFailed('Account not provisioned. Please contact an Administrator to enroll.')
+
+        return (user, None)
