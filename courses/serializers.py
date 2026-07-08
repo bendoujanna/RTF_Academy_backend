85c9a8d8d9b8c5e0f888b05a700b50ae2166a7f9
nakuwa23 <b.nakuwa@alustudent.com>
2026-07-08 15:20:38 +0200

    Prevent destructive module deletion on course update

--- courses/serializers.py
+++ courses/serializers.py
@@ -18,7 +18,7 @@ class LessonSerializer(serializers.ModelSerializer):
 class ModuleSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
     lesson_count = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Module
         fields = [
@@ -28,7 +28,7 @@ class ModuleSerializer(serializers.ModelSerializer):
             'lessons',
             'lesson_count',
         ]
-    
+
     def get_lesson_count(self, obj):
         return obj.lessons.count()
 
@@ -36,7 +36,7 @@ class ModuleSerializer(serializers.ModelSerializer):
 class CourseListSerializer(serializers.ModelSerializer):
     total_lessons = serializers.SerializerMethodField()
     module_count = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Course
         fields = [
@@ -50,10 +50,10 @@ class CourseListSerializer(serializers.ModelSerializer):
             'total_lessons',
             'module_count',
         ]
-    
+
     def get_total_lessons(self, obj):
         return Lesson.objects.filter(module__course=obj).count()
-    
+
     def get_module_count(self, obj):
         return obj.modules.count()
 
@@ -66,7 +66,7 @@ class CourseDetailSerializer(serializers.ModelSerializer):
 
     is_enrolled = serializers.SerializerMethodField()
     progress_percentage = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Course
         fields = [
@@ -83,82 +83,61 @@ class CourseDetailSerializer(serializers.ModelSerializer):
             'is_enrolled',
             'progress_percentage',
         ]
-    
+
     def get_total_lessons(self, obj):
         return Lesson.objects.filter(module__course=obj).count()
-    
+
     def get_module_count(self, obj):
         return obj.modules.count()
-    
+
     def get_is_enrolled(self, obj):
         request = self.context.get('request')
         if request and request.user and request.user.is_authenticated:
             return False
         return False
-    
+
     def get_progress_percentage(self, obj):
         request = self.context.get('request')
         if request and request.user and request.user.is_authenticated:
             return 0
         return 0
-    
+
 
 class CourseCreateUpdateSerializer(serializers.ModelSerializer):
-   modules = ModuleSerializer(many=True, required=False)
-  
-   class Meta:
-       model = Course
-       fields = [
-           'id',
-           'title',
-           'description',
-           'thumbnail_url',
-           'is_published',
-           'modules',
-       ]
-       read_only_fields = ['id', 'created_at', 'updated_at']
-  
-   def create(self, validated_data):
-       modules_data = validated_data.pop('modules', [])
+    modules = ModuleSerializer(many=True, required=False)
 
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'title',
+            'description',
+            'thumbnail_url',
+            'is_published',
+            'modules',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
-       course = Course.objects.create(**validated_data)
+    def create(self, validated_data):
+        modules_data = validated_data.pop('modules', [])
 
+        course = Course.objects.create(**validated_data)
 
-       for module_data in modules_data:
-           lessons_data = module_data.pop('lessons', [])
+        for module_data in modules_data:
+            lessons_data = module_data.pop('lessons', [])
 
+            module = Module.objects.create(course=course, **module_data)
 
-           module = Module.objects.create(course=course, **module_data)
+            for lesson_data in lessons_data:
+                Lesson.objects.create(module=module, **lesson_data)
 
+        return course
 
-           for lesson_data in lessons_data:
-               Lesson.objects.create(module=module, **lesson_data)
+    def update(self, instance, validated_data):
+        validated_data.pop('modules', None)  # Ignore nested modules on update
 
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
 
-       return course
-  
-   def update(self, instance, validated_data):
-       modules_data = validated_data.pop('modules', None)
-
-
-       for attr, value in validated_data.items():
-           setattr(instance, attr, value)
-       instance.save()
-
-
-       if modules_data is not None:
-           # Replacing nested modules keeps writes deterministic but resets nested IDs.
-           instance.modules.all().delete()
-
-
-           for module_data in modules_data:
-               lessons_data = module_data.pop('lessons', [])
-               module = Module.objects.create(course=instance, **module_data)
-
-
-               for lesson_data in lessons_data:
-                   Lesson.objects.create(module=module, **lesson_data)
-
-
-       return instance
+        return instance
