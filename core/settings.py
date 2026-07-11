abd7ddf98e1f4876515de38d59c30b6666a1f9d9
bendoujanna <b.vitalina@alustudent.com>
2026-07-11 18:49:01 +0200

    fix: configure whitenoise to serve static files in production

--- core/settings.py
+++ core/settings.py
@@ -170,7 +170,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Django REST Framework Global Settings
 REST_FRAMEWORK = {
